732b994e5306192b369d2f249ec014e7e105ba2b
aaa fff <aaa@ff.com>
2024-12-15 10:48:54 +0300

    changed tags

--- src/test/java/ibs/TestRunner.java
+++ src/test/java/ibs/TestRunner.java
@@ -7,9 +7,8 @@ import static io.cucumber.core.options.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@All"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value ="src/test/resources/feature"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value ="ibs/steps"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value ="io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
